7bbcdb9bb7349a97a7f3602b41ca2284b3f0b0ce
Windsor Wallaby <david@projectmentor.org>
2017-10-08 20:57:44 -0400

    downgrade src/AwsFacade

--- src/AwsFacade.php
+++ src/AwsFacade.php
@@ -1,16 +1,25 @@
-<?php namespace Aws\Laravel;
+<?php
 
-use Aws\AwsClientInterface;
+namespace Aws\Laravel;
+
+//REMOVE DOWNGRADED
+//use Aws\AwsClientInterface;
+//END REMOVE
+
+use Aws\Common\Client\AwsClientInterface;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * Facade for the AWS service
  *
- * @method static AwsClientInterface createClient($name, array $args = []) Get a client from the service builder.
+ * //REMOVE DOWNGRADED
+ * //method static AwsClientInterface createClient($name, array $args = []) Get a client from the service builder.
+ * //END REMOVE
+ *
+ * @method static AwsClientInterface get($name, $throwAway = false) Get a client from the service builder
  */
 class AwsFacade extends Facade
 {
-
     /**
      * Get the registered name of the component.
      *
@@ -20,5 +29,4 @@ class AwsFacade extends Facade
     {
         return 'aws';
     }
-
 }
